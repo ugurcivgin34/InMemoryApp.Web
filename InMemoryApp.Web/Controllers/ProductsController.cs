df53ca2ea0f821e5f219f10fa436fc6eb5c40d6f
uocivgin <uocivgin@ISBARGNT271007>
2023-04-18 15:34:10 +0300

    Add project files.

---
+++ InMemoryApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private IMemoryCache _memoryCache;
+
+        public ProductsController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            ////1.yol
+            //if (String.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            //{
+            //    _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            //}
+            ////2.yol
+
+            //if (!_memoryCache.TryGetValue("zaman", out string zamancache))
+            //{
+            //    _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            //}
+
+            MemoryCacheEntryOptions options = new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(1),
+
+                SlidingExpiration = TimeSpan.FromSeconds(10)
+            };
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
+
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            //"zaman" adlı anahtarın var olup olmadığı kontrol edilir. Eğer anahtar önbellekte mevcut değilse, lambda ifadesi kullanılarak yeni bir değer oluşturulur ve önbelleğe eklenir. Lambda ifadesi, şu anki tarih ve saat bilgisini alır ve bir dize olarak biçimlendirir.
+            //_memoryCache.GetOrCreate<string>("zaman", entry =>
+            //{
+            //    return DateTime.Now.ToString();
+            //});
+
+
+            //TryGetValue yöntemi kullanılarak, _memoryCache önbelleği içinde "zaman" anahtarına sahip bir öğe var mı diye kontrol edilir. Bu yöntem, önbellekteki değeri almak için kullanılan bir önbellek erişim yöntemidir. Eğer "zaman" anahtarına sahip bir öğe varsa, değer out parametresi aracılığıyla getirilir ve bu değer daha sonra kullanılmayacak bir _ değişkenine atanır. Bu değişken, _memoryCache'in Get yönteminde kullanılacak yer tutucu bir değişkendir.
+            _memoryCache.TryGetValue("zaman", out string _);
+            ViewBag.zaman = _memoryCache.Get<string>("zaman");
+            return View();
+        }
+    }
+}
---
+++ InMemoryApp.Web/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddMemoryCache(); //InMemory cach kullanmak için ekledik
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
