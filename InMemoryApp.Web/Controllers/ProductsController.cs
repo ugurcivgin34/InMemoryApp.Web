d98d041f6f7b6b9cc588b50f534ffb44c09c6656
uocivgin <uocivgin@ISBARGNT271007>
2023-05-04 15:45:45 +0300

    Complext types caching

--- InMemoryApp.Web/Controllers/ProductsController.cs
+++ InMemoryApp.Web/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InMemoryApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace InMemoryApp.Web.Controllers
@@ -49,6 +50,9 @@ namespace InMemoryApp.Web.Controllers
 
             _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
 
+            Product p = new Product { Id = 1, Name = "Kalem", Price = 200 };
+            _memoryCache.Set<Product>("product:1", p);
+            _memoryCache.Set<double>("money", 100.99);
 
             return View();
         }
@@ -67,6 +71,7 @@ namespace InMemoryApp.Web.Controllers
             _memoryCache.TryGetValue("callback", out string callback);
             ViewBag.zaman = zamancache;
             ViewBag.callback = callback;
+            ViewBag.product = _memoryCache.Get<Product>("product:1");
             return View();
         }
     }
